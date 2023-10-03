df83ae7306395470cf372cb6035482a7ba88d4fe
OveaTrint <oveatrints@gmail.com>
2023-10-03 14:51:08 +0100

    updated function

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 name = 'Kamal Baiyewu'
 print(f'Hello {name} ')
+
 def number(**num):
     add = num['num1'] + num['num2'] + num['num3']
     print(add)
+
+
 number(num1 = 1, num2 = 3, num3 = 4)
+
+def anything(food = 'beans'):
+    print(f'I love {food}')
+
+anything()
+anything('plantain')
+anything('rice')
